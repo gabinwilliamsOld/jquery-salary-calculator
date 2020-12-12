3793bb1671e324b6524778e11d6356952a7266d9
Gabin Williams <gabinwilliams@gmail.com>
2020-12-12 15:36:09 +0100

    Created function to grab input information and make new obj.  Also created global employee array

--- client.js
+++ client.js
@@ -1,9 +1,30 @@
 
 console.log('js loaded!');
 
+//Global employee array
+let employees = [];
+
 $(document).ready(readyHandle);
 
 function readyHandle(){
 
   console.log('jQuery is ready!');
+
+//Click listener!
+$('#submitButton').on('click', addEmployee);
+
+};// end readyHandle
+
+
+
+
+function addEmployee() {
+  let newEmployee = {
+    first: $('#firstIn').val(),
+    last: $('#lastIn').val(),
+    id: $('#idIn').val(),
+    title: $('#titleIn').val(),
+    salary: $('#salaryIn').val()
+};
+  console.log(newEmployee);
 };
